eacfa41d96778e19085d70d1428603f88a8d251d
wdingsoft <wdingbox@gmail.com>
2021-03-30 00:43:15 -0400

    work ok

---
+++ thesis/weid/htmdoc/proj/TheFamilialVocabulary/doc_files/Charts/FamilialVocabStat.json.js
@@ -0,0 +1,266 @@
+var FamilialVocabStat = {
+    "H120": {
+        "Hebrew": "אָדָם",
+        "Count": [
+            "38",
+            "552"
+        ],
+        "Eng": "mankind"
+    },
+    "H2145": {
+        "Hebrew": "זָכָר",
+        "Count": [
+            "14",
+            "81"
+        ],
+        "Eng": "male"
+    },
+    "H5347 ": {
+        "Hebrew": "נְקֵבָה",
+        "Count": [
+            "6",
+            "22"
+        ],
+        "Eng": "female"
+    },
+    "H802": {
+        "Hebrew": "אִשָּׁה",
+        "Count": [
+            "132",
+            "780"
+        ],
+        "Eng": "woman"
+    },
+    "H376": {
+        "Hebrew": "אִישׁ",
+        "Count": [
+            "98",
+            "1639"
+        ],
+        "Eng": "man"
+    },
+    "H1": {
+        "Hebrew": "אָב",
+        "Count": [
+            "170",
+            "1215"
+        ],
+        "Eng": "father"
+    },
+    "H517": {
+        "Hebrew": "אֵם",
+        "Count": [
+            "21",
+            "115"
+        ],
+        "Eng": "mother"
+    },
+    "H1121": {
+        "Hebrew": "בֵּן",
+        "Count": [
+            "306",
+            "4906"
+        ],
+        "Eng": "son"
+    },
+    "H2332": {
+        "Hebrew": "חַוָּה",
+        "Count": [
+            "2",
+            "2"
+        ],
+        "Eng": "Eve"
+    },
+    "H251": {
+        "Hebrew": "אָח",
+        "Count": [
+            "155",
+            "629"
+        ],
+        "Eng": "brother"
+    },
+    "H269": {
+        "Hebrew": "אָחוֹת",
+        "Count": [
+            "23",
+            "114"
+        ],
+        "Eng": "sister"
+    },
+    "H1323": {
+        "Hebrew": "בַּת",
+        "Count": [
+            "90",
+            "588"
+        ],
+        "Eng": "daughter"
+    },
+    "H5220": {
+        "Hebrew": "נֶכֶד",
+        "Count": [
+            "1",
+            "3"
+        ],
+        "Eng": "nephew"
+    },
+    "H6370": {
+        "Hebrew": "פִּילֶגֶשׁ",
+        "Count": [
+            "4",
+            "37"
+        ],
+        "Eng": "concubine"
+    },
+    "H1733": {
+        "Hebrew": "דּוֹדָה",
+        "Count": [
+            "0",
+            "3"
+        ],
+        "Eng": "aunt"
+    },
+    "H1730": {
+        "Hebrew": "דּוֹד",
+        "Count": [
+            "0",
+            "61"
+        ],
+        "Eng": "uncle"
+    },
+    "G4773": {
+        "Hebrew": "συγγενής",
+        "Count": [
+            "0",
+            "0"
+        ],
+        "Eng": "cousin"
+    },
+    "G3125": {
+        "Hebrew": "μάμμη",
+        "Count": [
+            "0",
+            "0"
+        ],
+        "Eng": "grandmother"
+    },
+    "-": {
+        "Hebrew": "-",
+        "Count": [
+            0,
+            0
+        ],
+        "Eng": "niece"
+    },
+    "": {
+        "Hebrew": "",
+        "Count": [
+            0,
+            0
+        ],
+        "Eng": ""
+    },
+    "H5828": {
+        "Hebrew": "עֵזֶר",
+        "Count": [
+            "2",
+            "21"
+        ],
+        "Eng": "help"
+    },
+    "H3206": {
+        "Hebrew": "יֶלֶד",
+        "Count": [
+            "17",
+            "89"
+        ],
+        "Eng": "child"
+    },
+    "H7223": {
+        "Hebrew": "רִאשׁוֹן",
+        "Count": [
+            "10",
+            "185"
+        ],
+        "Eng": "ancestor"
+    },
+    "H4940": {
+        "Hebrew": "מִשְׁפָּחָה",
+        "Count": [
+            "12",
+            "301"
+        ],
+        "Eng": "family"
+    },
+    "H2056": {
+        "Hebrew": "וָלָד",
+        "Count": [
+            "1",
+            "2"
+        ],
+        "Eng": "child"
+    },
+    "H3618": {
+        "Hebrew": "כַּלָּה",
+        "Count": [
+            "4",
+            "34"
+        ],
+        "Eng": "bride"
+    },
+    "H7872": {
+        "Hebrew": "שֵׂיבָה",
+        "Count": [
+            "5",
+            "19"
+        ],
+        "Eng": "old"
+    },
+    "H2860": {
+        "Hebrew": "חָתָן",
+        "Count": [
+            "2",
+            "20"
+        ],
+        "Eng": "bridegroom"
+    },
+    "H5209": {
+        "Hebrew": "נִין",
+        "Count": [
+            "1",
+            "3"
+        ],
+        "Eng": "offspring"
+    },
+    "H8380": {
+        "Hebrew": "תְּאוֹמִים",
+        "Count": [
+            "2",
+            "4"
+        ],
+        "Eng": "twin"
+    },
+    "H2859": {
+        "Hebrew": "חָתַן",
+        "Count": [
+            "1",
+            "33"
+        ],
+        "Eng": "marry"
+    },
+    "H1397": {
+        "Hebrew": "גֶּבֶר",
+        "Count": [
+            "0",
+            "68"
+        ],
+        "Eng": "man"
+    },
+    "H5768": {
+        "Hebrew": "עוֹלֵל",
+        "Count": [
+            "0",
+            "20"
+        ],
+        "Eng": "child"
+    }
+}
