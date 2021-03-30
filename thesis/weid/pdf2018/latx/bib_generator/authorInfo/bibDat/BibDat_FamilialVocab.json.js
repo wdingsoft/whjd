86d498c102985f9bf48b8d6719c82d5edbf17f7a
wdingsoft <wdingbox@gmail.com>
2021-03-30 19:46:36 -0400

    update

--- thesis/weid/pdf2018/latx/bib_generator/authorInfo/bibDat/BibDat_FamilialVocab.json.js
+++ thesis/weid/pdf2018/latx/bib_generator/authorInfo/bibDat/BibDat_FamilialVocab.json.js
@@ -1,6 +1,112 @@
 var BibDat_FamilialVocab = BibDat =
 
 {
+    "article":{
+        "doctype":"article",
+        "author":"Henrich, Joseph and Muthukrishna, Michael",
+        "authorinfo":"",
+        "title":"",
+        "journal":"Annual Review of Psychology",
+        "vol":"Vol 72",
+        "year":"2021",
+        "pape":"p207-240",
+        "publisher":"",
+        "isbn":"",
+        "url-1":"",
+        "abstract":": Humans are an ultrasocial species. This sociality, however, cannot be fully explained by the canonical approaches found in evolutionary biology, psychology, or economics. Understanding our unique social psychology requires accounting not only for the breadth and intensity of human cooperation but also for the variation found across societies, over history, and among behavioral domains. Here, we introduce an expanded evolutionary approach that considers how genetic and cultural evolution, and their interaction, may have shaped both the reliably developing features of our minds and the well-documented differences in cultural psychologies around the globe. We review the major evolutionary mechanisms that have been proposed to explain human cooperation, including kinship, reciprocity, reputation, signaling, and punishment; we discuss key culture–gene coevolutionary hypotheses, such as those surrounding self-domestication and norm psychology; and we consider the role of religions and marriage systems. Empirically, we synthesize experimental and observational evidence from studies of children and adults from diverse societies with research among nonhuman primates. [ABSTRACT FROM AUTHOR] DOI: 10.1146/annurev-psych-081920-042106. (AN: 147923563)",
+        "quotes":{
+            "p121":""
+        }
+    },
+
+    "article":{
+        "doctype":"article",
+        "author":"",
+        "authorinfo":"",
+        "title":"",
+        "journal":"",
+        "vol":"",
+        "year":"",
+        "pape":"",
+        "publisher":"",
+        "isbn":"",
+        "url-1":"",
+        "abstract":"",
+        "quotes":{
+            "p121":""
+        }
+    },
+
+    "article":{
+        "doctype":"article",
+        "author":"",
+        "authorinfo":"",
+        "title":"",
+        "journal":"",
+        "vol":"",
+        "year":"",
+        "pape":"",
+        "publisher":"",
+        "isbn":"",
+        "url-1":"",
+        "abstract":"",
+        "quotes":{
+            "p121":""
+        }
+    },
+
+    "article":{
+        "doctype":"article",
+        "author":"",
+        "authorinfo":"",
+        "title":"",
+        "journal":"",
+        "vol":"",
+        "year":"",
+        "pape":"",
+        "publisher":"",
+        "isbn":"",
+        "url-1":"",
+        "abstract":"",
+        "quotes":{
+            "p121":""
+        }
+    },
+
+    "article":{
+        "doctype":"article",
+        "author":"",
+        "authorinfo":"",
+        "title":"",
+        "journal":"",
+        "vol":"",
+        "year":"",
+        "pape":"",
+        "publisher":"",
+        "isbn":"",
+        "url-1":"",
+        "abstract":"",
+        "quotes":{
+            "p121":""
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     "webpage": {
         "doctype": "webpage",
         "author": "",
@@ -11,6 +117,7 @@ var BibDat_FamilialVocab = BibDat =
         "Urldate": "",
         "url": "",
         "notes": "",
+        "abstract":"",
         "quotes":{
             "":""
         }
@@ -36,6 +143,7 @@ var BibDat_FamilialVocab = BibDat =
         "title":"",
         "year":"",
         "url":"",
+        "abstract":"",
         "quotes":{
             "1":""
         }
@@ -52,6 +160,7 @@ var BibDat_FamilialVocab = BibDat =
         "publisher":"",
         "isbn":"",
         "url-1":"",
+        "abstract":"",
         "quotes":{
             "p121":""
         }
--- thesis/weid/pdf2018/latx/bib_generator/authorInfo/bibDat/commonTemplate.json.js
+++ thesis/weid/pdf2018/latx/bib_generator/authorInfo/bibDat/commonTemplate.json.js
@@ -1,4 +1,4 @@
-var BibDat=
+var BibDat =
 {
     "webpage": {
         "doctype": "webpage",
@@ -10,98 +10,101 @@ var BibDat=
         "Urldate": "",
         "url": "",
         "notes": "",
-        "quotes":{
-            "":""
+        "abstract": "",
+        "quotes": {
+            "": ""
         }
     },
-    "book":{
-        "doctype":"book",
-        "author":"",
-        "booktitle":"",
-        "year":"",
-        "publisher":"",
-        "address":"",
-        "url":"",
-        "url-1":"",
-        "isbn":"",
-        "abstract":"",
-        "quotes":{
-            "p1":""
+    "book": {
+        "doctype": "book",
+        "author": "",
+        "booktitle": "",
+        "year": "",
+        "publisher": "",
+        "address": "",
+        "url": "",
+        "url-1": "",
+        "isbn": "",
+        "abstract": "",
+        "quotes": {
+            "p1": ""
         }
     },
-    "report":{
-        "doctype":"report",
-        "author":"",
-        "title":"",
-        "year":"",
-        "url":"",
-        "quotes":{
-            "1":""
+    "report": {
+        "doctype": "report",
+        "author": "",
+        "title": "",
+        "year": "",
+        "url": "",
+        "abstract": "",
+        "quotes": {
+            "1": ""
         }
     },
-    "article":{
-        "doctype":"article",
-        "author":"",
-        "authorinfo":"",
-        "title":"",
-        "journal":"",
-        "vol":"",
-        "year":"",
-        "pape":"",
-        "publisher":"",
-        "isbn":"",
-        "url-1":"",
-        "quotes":{
-            "p121":""
+    "article": {
+        "doctype": "article",
+        "author": "",
+        "authorinfo": "",
+        "title": "",
+        "journal": "",
+        "vol": "",
+        "year": "",
+        "pape": "",
+        "publisher": "",
+        "isbn": "",
+        "url-1": "",
+        "abstract": "",
+        "quotes": {
+            "p121": ""
         }
     },
-    
-    "masterthesis":{
-        "doctype":"masterthesis",
-        "author":" ",
-        "authorinfo":"",
-        "booktitle":"",
-        "title":" ",
-        "year":" ",
-        "publisher":" ",
-        "isbn":"",
-        "journal":"",
-        "url-1":"f ",
-        "abstract":"",
-        "quotes":{
-            "":""
+
+    "masterthesis": {
+        "doctype": "masterthesis",
+        "author": " ",
+        "authorinfo": "",
+        "booktitle": "",
+        "title": " ",
+        "year": " ",
+        "publisher": " ",
+        "isbn": "",
+        "journal": "",
+        "url-1": "f ",
+        "abstract": "",
+        "quotes": {
+            "": ""
         }
     },
-    "phdthesis":{
-        "doctype":"phdthesis",
-        "author":" ",
-        "authorinfo":"",
-        "booktitle":"",
-        "title":" ",
-        "year":" ",
-        "publisher":" ",
-        "isbn":"",
-        "journal":"",
-        "url-1":"f ",
-        "abstract":"",
-        "quotes":{
-            "":""
+    "phdthesis": {
+        "doctype": "phdthesis",
+        "author": " ",
+        "authorinfo": "",
+        "booktitle": "",
+        "title": " ",
+        "year": " ",
+        "publisher": " ",
+        "isbn": "",
+        "journal": "",
+        "url-1": "f ",
+        "abstract": "",
+        "quotes": {
+            "": ""
         }
     },
-  
-    "book":{
-        "doctype":"book",
-        "author":"",
-        "booktitle":"",
-        "title":"",
-        "year":"",
-        "publisher":"",
-        "isbn":"",
-        "journal":"",
-        "url-1":"",
-        "abstract":"",
-        "quotes":{
-            "":""
+
+    "book": {
+        "doctype": "book",
+        "author": "",
+        "booktitle": "",
+        "title": "",
+        "year": "",
+        "publisher": "",
+        "isbn": "",
+        "journal": "",
+        "url-1": "",
+        "abstract": "",
+        "quotes": {
+            "": ""
         }
     },
 }
