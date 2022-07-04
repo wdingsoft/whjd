51f8ffa472fbc0c966dab62957b763119b24756f
wdingsoft <wdingbox@gmail.com>
2022-07-04 14:50:44 +0800

    Spirit soul stats

---
+++ biblestudy/my_papers/docs/spirit_soul_stats.js
@@ -0,0 +1,273 @@
+
+
+
+
+
+
+
+
+
+var BlueLetter_Frq = {
+    Ruah: {
+        "Gen": 11,
+        "Exo": 11,
+        "Num": 14,
+        "Deu": 2,
+        "Jos": 2,
+        "Jdg": 10,
+        "1Sa": 16,
+        "2Sa": 3,
+        "1Ki": 11,
+        "2Ki": 5,
+        "1Ch": 5,
+        "2Ch": 10,
+        "Ezr": 2,
+        "Neh": 2,
+        "Job": 31,
+        "Psa": 39,
+        "Pro": 21,
+        "Ecc": 24,
+        "Isa": 51,
+        "Jer": 18,
+        "Lam": 1,
+        "Eze": 52,
+        "Dan": 4,
+        "Hos": 7,
+        "Joe": 2,
+        "Amo": 1,
+        "Jon": 2,
+        "Mic": 3,
+        "Hab": 2,
+        "Hag": 4,
+        "Zec": 9,
+        "Mal": 3,
+    }
+    , 
+    Nefesh: {
+        "Gen": 43,
+        "Exo": 17,
+        "Lev": 60,
+        "Num": 50,
+        "Deu": 35,
+        "Jos": 16,
+        "Jdg": 10,
+        "Rth": 1,
+        "1Sa": 34,
+        "2Sa": 18,
+        "1Ki": 23,
+        "2Ki": 15,
+        "1Ch": 5,
+        "2Ch": 4,
+        "Est": 6,
+        "Job": 36,
+        "Psa": 144,
+        "Pro": 56,
+        "Ecc": 7,
+        "Sng": 7,
+        "Isa": 34,
+        "Jer": 63,
+        "Lam": 12,
+        "Eze": 42,
+        "Hos": 2,
+        "Amo": 3,
+        "Jon": 5,
+        "Mic": 3,
+        "Hab": 3,
+        "Hag": 1,
+        "Zec": 2,
+    }
+
+    , 
+    Elohim: {
+        "Gen": 219,
+        "Exo": 139,
+        "Lev": 53,
+        "Num": 27,
+        "Deu": 374,
+        "Jos": 76,
+        "Jdg": 73,
+        "Rth": 4,
+        "1Sa": 100,
+        "2Sa": 54,
+        "1Ki": 108,
+        "2Ki": 98,
+        "1Ch": 118,
+        "2Ch": 203,
+        "Ezr": 55,
+        "Neh": 70,
+        "Job": 17,
+        "Psa": 362,
+        "Pro": 5,
+        "Ecc": 40,
+        "Isa": 94,
+        "Jer": 145,
+        "Eze": 36,
+        "Dan": 22,
+        "Hos": 26,
+        "Joe": 11,
+        "Amo": 14,
+        "Jon": 16,
+        "Mic": 11,
+        "Nah": 1,
+        "Hab": 3,
+        "Zep": 5,
+        "Hag": 3,
+        "Zec": 11,
+        "Mal": 7,
+    },
+    Yhwy: {
+        "Gen": 163,
+        "Exo": 397,
+        "Lev": 311,
+        "Num": 396,
+        "Deu": 548,
+        "Jos": 223,
+        "Jdg": 173,
+        "Rth": 18,
+        "1Sa": 320,
+        "2Sa": 146,
+        "1Ki": 255,
+        "2Ki": 277,
+        "1Ch": 175,
+        "2Ch": 384,
+        "Ezr": 37,
+        "Neh": 17,
+        "Job": 32,
+        "Psa": 687,
+        "Pro": 87,
+        "Isa": 425,
+        "Jer": 712,
+        "Lam": 32,
+        "Eze": 217,
+        "Dan": 8,
+        "Hos": 46,
+        "Joe": 33,
+        "Amo": 60,
+        "Oba": 6,
+        "Jon": 26,
+        "Mic": 39,
+        "Nah": 13,
+        "Hab": 12,
+        "Zep": 33,
+        "Hag": 35,
+        "Zec": 132,
+        "Mal": 46,
+    }
+}
+
+
+
+var BlueLetterBibleCode_Stats_Init = {
+    "Gen": [],//     "Gen": "Gen",
+    "Exo": [],//     "Exo": "Exo",
+    "Lev": [],//     "Lev": "Lev",
+    "Num": [],//     "Num": "Num",
+    "Deu": [],//     "Deu": "Deu",
+    "Jos": [],//     "Jos": "Jos",//////
+    "Jdg": [],//     "Jug": "Jdg",//////
+    "Rth": [],//     "Rut": "Rut",
+    "1Sa": [],//     "1Sa": "1Sa",
+    "2Sa": [],//     "2Sa": "2Sa",
+    "1Ki": [],//     "1Ki": "1Ki",
+    "2Ki": [],//     "2Ki": "2Ki",
+    "1Ch": [],//     "1Ch": "1Ch",
+    "2Ch": [],//     "2Ch": "2Ch",
+    "Ezr": [],//     "Ezr": "Ezr",
+    "Neh": [],//     "Neh": "Neh",
+    "Est": [],//     "Est": "Est",
+    "Job": [],//     "Job": "Job",
+    "Psa": [],//     "Psm": "Psa",
+    "Pro": [],//     "Pro": "Pro",
+    "Ecc": [],//     "Ecc": "Ecc",
+    "Sng": [],//     "Son": "Son",//////
+    "Isa": [],//     "Isa": "Isa",
+    "Jer": [],//     "Jer": "Jer",
+    "Lam": [],//     "Lam": "Lam",
+    "Eze": [],//     "Eze": "Eze",
+    "Dan": [],//     "Dan": "Dan",
+    "Hos": [],//     "Hos": "Hos",
+    "Joe": [],//     "Joe": "Joe",
+    "Amo": [],//     "Amo": "Amo",
+    "Oba": [],//     "Oba": "Oba",
+    "Jon": [],//     "Jon": "Jon",
+    "Mic": [],//     "Mic": "Mic",
+    "Nah": [],//     "Nah": "Nah",
+    "Hab": [],//     "Hab": "Hab",
+    "Zep": [],//     "Zep": "Zep",
+    "Hag": [],//     "Hag": "Hag",
+    "Zec": [],//     "Zec": "Zec",
+    "Mal": [],//     "Mal": "Mal",
+    "Mat": [],//     "Mat": "Mat",
+    "Mar": [],//     "Mak": "Mar",
+    "Luk": [],//     "Luk": "Luk",
+    "Jhn": [],//     "Jhn": "Jhn",
+    "Act": [],//     "Act": "Act",
+    "Rom": [],//     "Rom": "Rom",
+    "1Co": [],//     "1Co": "1Co",
+    "2Co": [],//     "2Co": "2Co",
+    "Gal": [],//     "Gal": "Gal",
+    "Eph": [],//     "Eph": "Eph",
+    "Phl": [],//     "Phl": "Phl",
+    "Col": [],//     "Col": "Col",
+    "1Ts": [],//     "1Ts": "1Ts",
+    "2Ts": [],//     "2Ts": "2Ts",
+    "1Ti": [],//     "1Ti": "1Ti",
+    "2Ti": [],//     "2Ti": "2Ti",
+    "Tit": [],//     "Tit": "Tit",
+    "Phm": [],//     "Phm": "Phm",
+    "Heb": [],//     "Heb": "Heb",
+    "Jas": [],//     "Jas": "Jas",
+    "1Pe": [],//     "1Pe": "1Pe",
+    "2Pe": [],//     "2Pe": "2Pe",
+    "1Jn": [],//     "1Jn": "1Jn",
+    "2Jn": [],//     "2Jn": "2Jn",
+    "3Jn": [],//     "3Jn": "3Jn",
+    "Jud": [],//     "Jud": "Jud",
+    "Rev": [],//     "Rev": "Rev",
+};//BookChapterVerseMax
+
+
+
+
+var Stats_Viewer = {
+    FullView_Stats: {},
+    stats: function (key_words) {
+        var fullView_Stats = this.FullView_Stats
+        Object.keys(key_words).forEach(function (keyName) {
+            fullView_Stats[keyName] = JSON.parse(JSON.stringify(BlueLetterBibleCode_Stats_Init))
+            var obj = key_words[keyName]
+            Object.keys(obj).forEach(function (book) {
+                if(!fullView_Stats[keyName][book] ){alert(keyName+"="+book)}
+                fullView_Stats[keyName][book] = obj[book]
+            })
+        })
+    },
+
+    output_table: function () {
+        var fullView_Stats = this.FullView_Stats
+
+        var theader="<theader><tr><td></td><td></td>"
+        Object.keys(BlueLetter_Frq).forEach(function (keyword) {
+            theader += `<th>${keyword}</th>`
+        })
+        theader+="</tr></theader>"
+
+
+        var tab = `<table border='1'>${theader}<tbody>`
+        var idx=1
+        Object.keys(BlueLetterBibleCode_Stats_Init).forEach(function (book) {
+            var tr = `<tr><td>${idx++}</td><td>${book}</td>`
+            Object.keys(BlueLetter_Frq).forEach(function (keyword) {
+                var frq = fullView_Stats[keyword][book]
+                tr += `<td>${frq}</td>`
+            })
+            tr += "</tr>"
+            tab+=tr
+        })
+        tab+="</tbody></table>"
+        return tab;
+    }
+}
+
+//Stats_Viewer.stats(BlueLetter_Frq)
+//var tab = Stats_Viewer.output_table()
