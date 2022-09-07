3db80ffe285af7bb592e8d65c1e3f63e1a55a077
wdingsoft <wdingbox@gmail.com>
2022-09-07 04:14:47 -0700

    log

---
+++ calendars/js/DailyRecords.js
@@ -0,0 +1,23 @@
+var DailyRecords = 
+{
+    "21_12_08": "$20 ESL Gordon",
+    "21_12_09": "Hose ord",
+    "21_12_11": "Esl-last class",
+    "21_12_12": "Hose Delivery",
+    "21_12_19": "<br><div><br></div>",
+    "21_12_20": "8:15pm disciple training.",
+    "21_12_22": "5:00 flight alaska",
+    "21_12_29": "ioi<div><br></div>",
+    "22_01_01": "new year",
+    "22_01_08": "Esl",
+    "22_01_21": "vacc<div>oop</div>",
+    "22_01_24": "rest<div><br></div>",
+    "22_01_31": "2.4",
+    "22_06_02": "tets",
+    "22_09_02": "Seattle: arrived<div><br><div><br></div></div>",
+    "22_09_03": "hiking",
+    "22_09_04": "Moderna:Safeway",
+    "22_09_05": "less food, more sleep",
+    "22_09_06": "normal<div><br></div>",
+    "22_09_07": "test===="
+}
