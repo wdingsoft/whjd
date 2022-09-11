6323ea38033a218203d09633f086e28def8cb4c5
wdingsoft <wdingbox@gmail.com>
2022-09-10 23:02:48 -0700

    modified:   ../calendars/js/DailyRecords.js

--- calendars/js/DailyRecords.js
+++ calendars/js/DailyRecords.js
@@ -16,7 +16,7 @@ var DailyRecords =
     "22_06_02": "tets",
     "22_09_02": "Seattle: arrived<div><br><div><br></div></div>",
     "22_09_03": "hiking",
-    "22_09_04": "Moderna:Safeway",
+    "22_09_04": "Moderna:Safeway<div>VitaminB</div>",
     "22_09_05": "Reaction: less food, more sleep",
     "22_09_06": "normal<div>痰 phlegm/sputum/Spittle </div>",
     "22_09_07": "throat sounds but no pain or uncomfort<div>hiking 3 hrs</div>",
