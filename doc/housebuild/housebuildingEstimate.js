2323c725ee9bc0ecfc159aa0c0d8a21c16e9c57c
wdingsoft <wdingbox@gmail.com>
2022-09-21 14:24:39 -0700

    new file:   HouseBuildingEstimate.xlsx new file:   housebuild/housebuildingEstimate.js modified:   index.htm

---
+++ doc/housebuild/housebuildingEstimate.js
@@ -0,0 +1,10 @@
+
+80,000	Buying And Prepping Land	
+7,200	Drawing Up Plans And Getting Permits	
+14,500	Prepping And Building The Foundation	
+35,000	Framing The Home	
+50,000	Installing External Elements	
+52,500	Installing Plumbing And Electrical 	
+112,500	Adding Interior Finishes	
+20,000	Finalizing Additions	
+10,000	Other Considerations For Building A House	
