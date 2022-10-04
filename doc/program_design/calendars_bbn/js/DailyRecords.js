7b22a49dad2830d3e1a3e6bbfbad97ff79cac22c
wdingsoft <wdingbox@gmail.com>
2022-10-04 15:06:02 -0400

    modified:   ../index.htm new file:   BBN_GoldenVerse.htm new file:   calendars_bbn/Jewish_Festivals.htm new file:   calendars_bbn/calendar3yr.htm new file:   calendars_bbn/jq/jquery.js new file:   calendars_bbn/js/DailyRecords.js new file:   index.htm

---
+++ doc/program_design/calendars_bbn/js/DailyRecords.js
@@ -0,0 +1,14 @@
+var DailyRecords =
+{
+    "22_09_02": "Seattle: arrived<div><br><div><br></div></div>",
+    "22_09_03": "hiking",
+    "22_09_04": "Moderna:Safeway<div>VitaminB</div><div>RoomTemp=90</div>",
+    "22_09_05": "Reaction: less food, more sleep",
+    "22_09_06": "normal<div>痰 phlegm/sputum/Spittle </div>",
+    "22_09_07": "throat sounds but no pain or uncomfort<div>hiking 3 hrs</div>",
+    "22_09_08": "hiking 3hr<div>RoomTemp=97</div>",
+    "22_09_09": "Timberlake Park, 5 hrs 5 mi.",
+    "22_09_10": "Golden Beach; Botanical Garden.&nbsp;",
+    "22_09_11": "crossroad church:Heb10:25",
+    "22_09_27": "flight 2 atlanta"
+}
