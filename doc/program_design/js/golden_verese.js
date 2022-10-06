3536d790a46a99e4b40e570fb7292ac1648185bf
wdingsoft <wdingbox@gmail.com>
2022-10-05 23:40:15 -0400

    modified:   GoldenVerse.htm modified:   js/golden_verese.js

--- doc/program_design/js/golden_verese.js
+++ doc/program_design/js/golden_verese.js
@@ -1,5 +1,6 @@
 var golden_verese = {
     "Eph2:10":["我们原是他的工作、在基督耶稣里造成的、为要叫我们行善、就是　神所预备叫我们行的。",""],
     "Pro15:21":["无知的人、以愚妄为乐．聪明的人、按正直而行。",""],
+    "Gal5:16":["我说、你们当顺着圣灵而行、就不放纵肉体的情欲了. ", "So I say, live by the Spirit, and you will not gratify the desires of the sinful nature.(Gal5:16 NIV) " ]
 }
 
