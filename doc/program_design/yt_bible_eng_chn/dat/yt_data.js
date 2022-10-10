a170243a3a923db7a67192943c0e0f10855bf1b9
wdingsoft <wdingbox@gmail.com>
2022-10-10 18:19:40 -0400

    new file:   ../dat/yt_data.js new file:   ../yt_bible_chinese_english.htm new file:   jq/jquery-2_1_3.min.js new file:   jq/yt_iframe_api.js new file:   js/yt_iframe_api.js new file:   yt_iframe_videoID.htm new file:   yt_sam_main.htm new file:   yt_sam_speed.htm

---
+++ doc/program_design/yt_bible_eng_chn/dat/yt_data.js
@@ -0,0 +1,775 @@
+
+
+var yt_data = {
+
+    "https://www.youtube.com/watch?v=pwb4qspOHTY&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=1&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        [
+            "读《圣经》,学英文: 创世记第一章：神创造天地万物", { startSeconds: 260 }
+        ],
+
+
+    "https://www.youtube.com/watch?v=XzGF5N53_7E&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=2&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        [
+            "读《圣经》,学英文: 创世记第二章：立伊甸园 为男人造配偶", {}
+        ],
+
+
+    "https://www.youtube.com/watch?v=xTHAzmoMX7E&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=3&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        [
+            "读《圣经》,学英文: 创世记第三章：始祖被诱惑 违背神的命令受咒诅 逐出伊甸园", {},
+        ],
+
+    "https://www.youtube.com/watch?v=ntEvmVXeTxI&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=4&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第四章：生该隐和亚伯， 该隐杀亚伯", {}],
+
+    "https://www.youtube.com/watch?v=XCsaZf5Abdg&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=5&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第五章：从亚当到挪亚", {},],
+
+    "https://www.youtube.com/watch?v=Tno212eZ45o&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=6&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第六章：邪恶的人类", {}],
+
+    "https://www.youtube.com/watch?v=IzicVd1rJ_I&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=7&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["(Error)读《圣经》,学英文: 创世记第七章：挪亚进方舟 洪水泛滥四十日", { start: "10" },],
+
+    "https://www.youtube.com/watch?v=h3bnpC6T6KA&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=8&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第八章：洪水消落 挪亚放乌鸦与鸽子出方舟 挪亚全家出方舟 挪亚筑坛献祭", {}],
+
+    "https://www.youtube.com/watch?v=oW6B4KvgO2I&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=9&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第九章：神赐福给挪亚， 神与挪亚立约， 咒诅迦南", {},],
+
+    "https://www.youtube.com/watch?v=kUrogXb_SzY&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=10&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十章: 闪、含、雅弗的后裔", {}],
+
+    "https://www.youtube.com/watch?v=GBn9JqViaNg&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=11&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十一章: 巴别塔和变乱口音、闪的后裔、他拉的后裔", {},],
+
+    "https://www.youtube.com/watch?v=0vTn6C8VgTs&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=12&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十二章: 神选召亚伯兰", {}],
+
+    "https://www.youtube.com/watch?v=NwuYgqBdtqs&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=13&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十三章: 亚伯兰往迦南, 亚伯兰与罗得分离", {},],
+
+    "https://www.youtube.com/watch?v=Kv6KCqoGcWk&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=14&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十四章: 四王与五王交战，亚伯兰救回罗得，麦基洗德祝福亚伯兰", {}],
+
+    "https://www.youtube.com/watch?v=06iC-oXriqc&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=15&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十五章: 神与亚伯兰立约", {},],
+
+    "https://www.youtube.com/watch?v=GmpOm-Oo1kk&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=16&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十六章: 撒莱与夏甲，神给夏甲的应许", {}],
+
+    "https://www.youtube.com/watch?v=u2apnmt_rbI&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=17&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十七章: 神更改亚伯兰的名为亚伯拉罕，始定割礼，神更改撒莱的名为沙拉，应许亚伯拉罕生以撒", {},],
+
+    "https://www.youtube.com/watch?v=Kpy-dMIiYqM&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=18&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十八章: 亚伯拉罕接待天使，应许撒拉生子，神将灭所多玛蛾摩拉，亚伯拉罕为所多玛代求", {}],
+
+    "https://www.youtube.com/watch?v=O43JoEmzCmI&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=19&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第十九章: 罗得接待二天使，引领罗得避灾，毁灭所多玛蛾摩拉", {},],
+
+    "https://www.youtube.com/watch?v=EOhSc24zoOg&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=20&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十章:亚伯拉罕和亚比 米勒", {}],
+
+    "https://www.youtube.com/watch?v=zn6vzVKfYKo&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=21&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十一章:以撒出生，夏甲和她儿子以实玛利被逐，亚伯拉罕与亚比 米勒立约", {},],
+
+    "https://www.youtube.com/watch?v=yS4q2JUKbZk&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=22&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十二章:神试验亚伯拉罕", {}],
+
+    "https://www.youtube.com/watch?v=Ux2RM9fspuA&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=23&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十三章:撒拉死亚伯拉罕买坟地，埋葬撒拉", {},],
+
+    "https://www.youtube.com/watch?v=eAU8TanK3bg&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=24&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十四章:亚伯拉罕遣仆为子娶妻，拉班迎接亚伯拉罕的仆人，仆人述说来意，拉班和彼土利允诺，以撒娶利百加为妻", {}],
+
+    "https://www.youtube.com/watch?v=huLb7t-PMkA&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=25&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["读《圣经》,学英文: 创世记第二十五章:亚伯拉罕的其他后裔，亚伯拉罕的死和埋葬，以实玛利的后裔，以扫和牙雅各出生，以扫出卖长子名分", {},],
+
+    "https://www.youtube.com/watch?v=GvM0avTRPDU&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=26&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["旧约创世记第二十六章: 以撒住在基拉尔", {}],
+
+    "https://www.youtube.com/watch?v=S7UJqKEXUwQ&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=27&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["创世记第二十七章：雅各计骗父的祝福/以扫痛哭求父祝福/以扫怨恨雅各欲杀他", {},],
+
+    "https://www.youtube.com/watch?v=HqVhLAl-KpM&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=28&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第二十八章：以撒遣雅各娶于舅家/雅各梦中得 神指示/雅各许愿", {}],
+
+    "https://www.youtube.com/watch?v=AmBTMnG-Qq4&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=29&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第二十九章：雅各遇拉结、雅各为拉结和利亚服事拉班、雅各的儿女", {},],
+
+    "https://www.youtube.com/watch?v=MNoQ75PIug8&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=30&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["圣经----创世记》第三十章：雅各与拉班定工价、雅各用计致富", {}],
+
+    "https://www.youtube.com/watch?v=RLBLvQuVgjQ&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=31&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十一章：雅各逃离拉班、拉班追赶雅各、雅各与拉班立约", {},],
+
+    "https://www.youtube.com/watch?v=ELPpW4LCOas&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=32&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十二章：雅各因恐怕而计晤以扫、雅各与天使摔交得胜", {}],
+
+    "https://www.youtube.com/watch?v=RLBLvQuVgjQ&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=31&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["圣经----创世记》第三十一章：雅各逃离拉班、拉班追赶雅各、雅各与拉班立约", {},],
+
+    "https://www.youtube.com/watch?v=ELPpW4LCOas&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=32&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十二章：雅各因恐怕而计晤以扫、雅各与天使摔交得胜", {}],
+
+    "https://www.youtube.com/watch?v=0JmW4PVEPH8&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=33&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十三章：雅各与以扫相会、雅各往示剑", {},],
+
+    "https://www.youtube.com/watch?v=6pV-ln91oIo&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=34&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十四章：雅各女儿被污其众子计杀示剑", {}],
+
+    "https://www.youtube.com/watch?v=8w1dj6pnL7k&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=35&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十五章：雅各往伯特利筑坛、拉结生便雅悯难产而死、以撒寿终", {},],
+
+    "https://www.youtube.com/watch?v=BiMAMTDGQeQ&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=36&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十六章：以扫的后裔、西珥的后裔、以东诸王", {}],
+
+    "https://www.youtube.com/watch?v=GPSB8XtM6oI&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=37&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十七章：约瑟的梦、约瑟被卖给以实玛利人", {},],
+
+    "https://www.youtube.com/watch?v=EZ735me89pM&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=38&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第三十九章：约瑟和波提乏、波提乏妻诬告约瑟、波提乏囚约瑟于监", {}],
+
+    "https://www.youtube.com/watch?v=J6sJzuj-mQ8&list=PLKO636bowN5-L5lZUD1kZC0c6IkhyjVuY&index=39&ab_channel=%E6%9B%B8%E8%B6%A3%E9%8A%80%E4%BE%86":
+        ["《圣经----创世记》第四十章：约瑟解酒政膳长的梦", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+    "":
+        ["", {},],
+
+    "":
+        ["", {}],
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
