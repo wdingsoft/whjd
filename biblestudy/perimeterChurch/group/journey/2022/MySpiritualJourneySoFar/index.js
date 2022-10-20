68c13ca7aa7d7f36f8adfb23ff6496baef7c727f
wdingsoft <wdingbox@gmail.com>
2022-10-20 17:36:50 -0400

    index.js

---
+++ biblestudy/perimeterChurch/group/journey/2022/MySpiritualJourneySoFar/index.js
@@ -0,0 +1,252 @@
+
+var obj = 
+{
+    "1964": [
+        "0",
+        "born in Beijing"
+    ],
+    "1965": [
+        "-29",
+        "sent to old town."
+    ],
+    "1966": [
+        "",
+        ""
+    ],
+    "1967": [
+        "",
+        ""
+    ],
+    "1968": [
+        "",
+        ""
+    ],
+    "1969": [
+        "",
+        ""
+    ],
+    "1970": [
+        "",
+        ""
+    ],
+    "1971": [
+        "",
+        ""
+    ],
+    "1972": [
+        "-38",
+        "back to Beijing w/ parents"
+    ],
+    "1973": [
+        "",
+        ""
+    ],
+    "1974": [
+        "",
+        ""
+    ],
+    "1975": [
+        "",
+        ""
+    ],
+    "1976": [
+        "",
+        ""
+    ],
+    "1977": [
+        "",
+        ""
+    ],
+    "1978": [
+        "",
+        ""
+    ],
+    "1979": [
+        "",
+        ""
+    ],
+    "1980": [
+        "",
+        ""
+    ],
+    "1981": [
+        "",
+        ""
+    ],
+    "1982": [
+        "-29",
+        "College life. during the period, influenced by Chinese Daoism(Laozi/Zhuangzi). (2) in 1986, I continued to pursue the master's degree major in vibration control."
+    ],
+    "1983": [
+        "",
+        ""
+    ],
+    "1984": [
+        "",
+        ""
+    ],
+    "1985": [
+        "",
+        ""
+    ],
+    "1986": [
+        "",
+        ""
+    ],
+    "1987": [
+        "",
+        ""
+    ],
+    "1988": [
+        "",
+        ""
+    ],
+    "1989": [
+        "-93",
+        "Tiananmen massacre. a tough decision to make between the master's degree diploma and the freedom, justice or truth, between the surviving and death. (2) It is a turning point of my original materialism, marxism and communism faith. The three love became three hatred in my life. My goal of life was to leave China forever. Trump is my reminder of such trauma."
+    ],
+    "1990": [
+        "",
+        ""
+    ],
+    "1991": [
+        "",
+        ""
+    ],
+    "1992": [
+        "-74",
+        "marriage. visited USA,"
+    ],
+    "1993": [
+        "",
+        ""
+    ],
+    "1994": [
+        "",
+        ""
+    ],
+    "1995": [
+        "-28",
+        "USA dream."
+    ],
+    "1996": [
+        "",
+        ""
+    ],
+    "1997": [
+        "",
+        ""
+    ],
+    "1998": [
+        "-24",
+        "First job"
+    ],
+    "1999": [
+        "",
+        ""
+    ],
+    "2000": [
+        "-11",
+        ""
+    ],
+    "2001": [
+        "-37",
+        "laid off (3) "
+    ],
+    "2002": [
+        "-48",
+        "stayed at home"
+    ],
+    "2003": [
+        "-91",
+        "(1) got citizenship.(2) laid off (3) family war broke out.(4) found a job in Ligos Software in Savannah GA."
+    ],
+    "2004": [
+        "50",
+        "baptized at ACCC."
+    ],
+    "2005": [
+        "",
+        ""
+    ],
+    "2006": [
+        "",
+        ""
+    ],
+    "2007": [
+        "",
+        ""
+    ],
+    "2008": [
+        "",
+        ""
+    ],
+    "2009": [
+        "",
+        ""
+    ],
+    "2010": [
+        "",
+        ""
+    ],
+    "2011": [
+        "",
+        ""
+    ],
+    "2012": [
+        "",
+        ""
+    ],
+    "2013": [
+        "",
+        ""
+    ],
+    "2014": [
+        "",
+        ""
+    ],
+    "2015": [
+        "",
+        ""
+    ],
+    "2016": [
+        "99",
+        "(1) quitted Emerson, left Huntsville to go to CIU seminary school.(2) visited Beijing in Dec 2019.(3) on the crossroad in the missionary: My vision: 'Leave your country, your people and your father's household and go to the land I will show you.' (Gen12:1 NIV)  "
+    ],
+    "2017": [
+        "",
+        ""
+    ],
+    "2018": [
+        "",
+        ""
+    ],
+    "2019": [
+        "",
+        ""
+    ],
+    "2020": [
+        "",
+        ""
+    ],
+    "2021": [
+        "",
+        ""
+    ],
+    "2022": [
+        "",
+        ""
+    ],
+    "2023": [
+        "",
+        ""
+    ],
+    "2024": [
+        "",
+        ""
+    ],
+    "2025": [
+        "",
+        ""
+    ]
+}
