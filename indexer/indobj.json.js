cf299c5454c6fd6f713127edca3c6e86f7097332
wdingsoft <wdingbox@gmail.com>
2022-10-21 23:49:51 -0400

    modified:   index.htm new file:   indobj.json.js new file:   indusr.js

---
+++ indexer/indobj.json.js
@@ -0,0 +1,31 @@
+var indobj = {
+    "./": [
+        {
+            "github_home.htm": 867
+        },
+        {
+            "index.htm": 7016
+        },
+        {
+            "index.html": 8451
+        },
+        {
+            "index.js": 24
+        },
+        {
+            "index.sh": 392
+        },
+        {
+            "index_watch.nod.js": 11031
+        },
+        {
+            "indobj.json.js": 456
+        },
+        {
+            "indusr.js": 24
+        },
+        {
+            "~make_htm.nd.js": 1587
+        }
+    ]
+}
